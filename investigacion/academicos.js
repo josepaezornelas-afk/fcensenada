f5ccd8c56c2f007171d4d3c6eb2060d49223ef7b
josepaezornelas-afk <jose.paez.ornelas@uabc.edu.mx>
2026-06-19 10:55:57 -0700

    banners de carreras

---
+++ investigacion/academicos.js
@@ -0,0 +1,132 @@
+const academicos = [
+    {
+        nombre: "Dr. Rafael Bello Bedoy",
+        categoria: "biologia",
+        imagen: "./docentes/panda.svg",
+        puesto: "Profesor Investigador",
+        lineas: "Fisiología Vegetal",
+        departamento: "Depto. de Biología",
+        telefono: "Ext. 64908",
+        correo: "biologia.fc@uabc.edu.mx"
+    },
+    {
+        nombre: "Dr. José Delgadillo Rodríguez",
+        categoria: "biologia",
+        imagen: "./docentes/panda.svg",
+        puesto: "Profesor Investigador",
+        lineas: "",
+        departamento: "Depto. de Matemáticas Aplicadas",
+        telefono: "Ext. 64945",
+        correo: "matematicas.fc@uabc.edu.mx"
+    },
+    {
+        nombre: "Dra. Selene Solorza Calderón",
+        categoria: "matematicas",
+        imagen: "./docentes/panda.svg",
+        puesto: "Profesora Investigadora",
+        lineas: "Interacción Humano-Computadora, Informática Médica, Computación Ubicua",
+        departamento: "Depto. de Matemáticas Aplicadas",
+        telefono: "Ext. 64921",
+        correo: "posgrado.fc@uabc.edu.mx"
+    },
+    {
+        nombre: "Dra. María Victoria Meza Kubo",
+        categoria: "computacion",
+        imagen: "./docentes/panda.svg",
+        puesto: "Profesora Investigadora",
+        lineas: "Interacción Humano-Computadora, Informática Médica, Computación Ubicua",
+        departamento: "Depto. de Computación",
+        telefono: "Ext. 64921",
+        correo: "posgrado.fc@uabc.edu.mx"
+    },
+    {
+        nombre: "Dr. Alberto Leopoldo Morán y Solares",
+        categoria: "computacion",
+        imagen: "./docentes/panda.svg",
+        puesto: "Profesor Investigador",
+        lineas: "Ingeniería de Software, Sistemas Distribuidos, Computación Colaborativa",
+        departamento: "Depto. de Ciencias Computacionales",
+        telefono: "Ext. 64900",
+        correo: "direccion.fc@uabc.edu.mx"
+    },
+    {
+        nombre: "Dr. Jose Ángel González Fraga",
+        categoria: "computacion",
+        imagen: "./docentes/panda.svg",
+        puesto: "Profesor Investigador",
+        lineas: "Inteligencia Artificial",
+        departamento: "Depto. de Ciencias Computacionales",
+        telefono: "Ext. 64907",
+        correo: "direccion.fc@uabc.edu.mx"
+    },
+    {
+        nombre: "Dra. Eloísa del Carmen García Canseco",
+        categoria: "computacion",
+        imagen: "./docentes/panda.svg",
+        puesto: "Profesor Investigador",
+        lineas: "Inteligencia Artificial",
+        departamento: "Depto. de Ciencias Computacionales",
+        telefono: "Ext. 64914",
+        correo: "direccion.fc@uabc.edu.mx"
+    },
+    {
+        nombre: "Dr. Everardo Gutiérrez López",
+        categoria: "computacion",
+        imagen: "./docentes/panda.svg",
+        puesto: "Profesor Investigador",
+        lineas: "Algoritmos Bioinspirados, Optimización Combinatoria, Análsis de Algoritmos",
+        departamento: "Depto. de Ciencias Computacionales",
+        telefono: "Ext. 64905",
+        correo: "direccion.fc@uabc.edu.mx"
+    },
+    {
+        nombre: "Dra. Alma Rocío Cabazos Marín",
+        categoria: "computacion",
+        imagen: "./docentes/panda.svg",
+        puesto: "Profesor Investigador",
+        lineas: "Algoritmos",
+        departamento: "Depto. de Ciencias Computacionales",
+        telefono: "Ext. 64910",
+        correo: "direccion.fc@uabc.edu.mx"
+    },
+    {
+        nombre: "Dra. Priscilla Elizabeth Iglesias Vázquez",
+        categoria: "fisica",
+        imagen: "./docentes/panda.svg",
+        puesto: "Profesora Investigadora",
+        lineas: "Modelado Matemático, Ecuaciones Diferenciales, Análisis Numérico",
+        departamento: "Depto. de Física",
+        telefono: "Ext. 64903",
+        correo: "subdirector.fc@uabc.edu.mx"
+    },
+    {
+        nombre: "Dr. Luis Pellegrin",
+        categoria: "cienciadedatos",
+        imagen: "./docentes/panda.svg",
+        puesto: "Profesor Investigador",
+        lineas: "Aprendizaje profundo",
+        departamento: "Depto. de Ciencia de Datos",
+        telefono: "Ext. 64903",
+        correo: "pellegrin@uabc.edu.mx"
+    },
+    {
+        nombre: "Dr. Raul Casillas Figueroa",
+        categoria: "cienciadedatos",
+        imagen: "./docentes/panda.svg",
+        puesto: "Profesor Investigador",
+        lineas: "Aprendizaje profundo",
+        departamento: "Depto. de Ciencia de Datos",
+        telefono: "Ext. 64903",
+        correo: "rcasillas@uabc.edu.mx"
+    },
+    {
+        nombre: "Dr. Israel Paez Ornelas",
+        categoria: "cienciadedatos",
+        imagen: "./docentes/panda.svg",
+        puesto: "Profesor Investigador",
+        lineas: "Informática de materiales",
+        departamento: "Depto. de Ciencia de Datos",
+        telefono: "Ext. 64903",
+        correo: "jose.paez.ornelas@uabc.edu.mx"
+    }
+];
